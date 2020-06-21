f2e5dcd1cc916d4e83ca476aae5776dcd8753650
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-06-21 14:32:18 +0530

    final commit

--- application.py
+++ application.py
@@ -6,35 +6,16 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/json", methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def sentimentAnalysis():
-
-  # if the document is submitted
-  if request.method=='POST':
-
-    req_data = request.get_json(force=True)
-    labscore=0
-    n = len(req_data['messages'])
-    for i in range(n):
-      message = req_data['messages'][i]
-
-      #this pre-fitted model is based on IMDB dataset
-      classifier = TextClassifier.load('en-sentiment')
-
-      #variable to store the input query ('document') by the user
-      inputQuery = message
-
-      #Sentence is a list of tokens, here, the input
-      sentence = Sentence(inputQuery)
-
-      #calling the .predict function on the sentence
-      classifier.predict(sentence)
-
-      label = sentence.labels[0]
-      labscore += ((label.score)*100)
-      labscore = labscore/n
-    response = {'result': label.value, 'score': "%.2f" % labscore }
-    return jsonify(response)
-  else:
-    return render_template('index.html')
-
+  req_data = request.get_json(force=True)
+  messages = req_data['messages']
+  final = ' '.join(messages)
+  classifier = TextClassifier.load('en-sentiment')
+  sentence = Sentence(final)
+  classifier.predict(sentence)
+  print('Sentiment: ', sentence.labels)
+  label = sentence.labels[0]
+  labscore = (label.score)*100
+  response = {'sentiment': label.value, 'score': "%.2f" % labscore}
+  return jsonify(response)
